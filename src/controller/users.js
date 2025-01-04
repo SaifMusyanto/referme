ca6d2b05e308e5b394dfca68c40f53f0a10c7044
SaifMusyanto <saifmusyanto@gmail.com>
2025-01-04 09:42:24 +0700

    add validation to UserController

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,5 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const { body, validationResult } = require('express-validator');
 
 // Controller untuk mendapatkan semua user
 const getAllUsers = async (req, res) => {
@@ -15,10 +16,23 @@ const getAllUsers = async (req, res) => {
 
 // Controller untuk menambahkan user baru
 const createNewUser = async (req, res) => {
-  try {
-    const { username, email, phone_number, password } = req.body;
+  const { username, email, phone_number, password } = req.body;
 
-    // Cek apakah email sudah ada di database
+  const userValidation = [
+    body('username').isString().notEmpty().withMessage('Username is required'),
+    body('email').isEmail().withMessage('Invalid email format').notEmpty().withMessage('Email is required'),
+    body('phone_number').isString().notEmpty().withMessage('Phone number is required'),
+    body('password').isStrongPassword().withMessage('Your password is not Strong enough, Use characters !@#$%^&*(* to get strong password').notEmpty().withMessage('Password is required').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+  ];
+
+  await Promise.all(userValidation.map(validation => validation.run(req)));
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
     const existingUser = await prisma.user.findUnique({
       where: { email },
     });
@@ -27,7 +41,6 @@ const createNewUser = async (req, res) => {
       return res.status(400).json({ error: 'Email already in use' });
     }
 
-    // Buat user baru jika email belum terdaftar
     const newUser = await prisma.user.create({
       data: { username, email, phone_number, password },
     });
@@ -40,14 +53,28 @@ const createNewUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-  const { username, phone_number, password } = req.body;
+  const { username, phone_number } = req.body;
+
+  const updateValidation = [
+    body('username')
+    .isString().withMessage('Username must be a string'),
+    body('phone_number')
+    .isString().withMessage('Phone number must be a string')
+  ];
+
+  await Promise.all(updateValidation.map(validation => validation.run(req)));
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
     try {
     const newProfile = await prisma.user.update({
       where: { user_id: parseInt(req.params.idUser) },
       data: {
         username: username,
         phone_number: phone_number,
-        password: password,
       },
     });
 
